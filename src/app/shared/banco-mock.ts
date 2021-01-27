56c0447a877635f677319357d1ba43c3edcdf893
Fernando Silveira <suporte.ti1993@hotmail.com>
2021-01-27 11:33:39 -0300

    atualizacao 27/01

--- src/app/shared/banco-mock.ts
+++ src/app/shared/banco-mock.ts
@@ -5,34 +5,34 @@ export const CAROUSEL:Carousel[] =
 [
     new Carousel("Gestante","Paola",
     "https://lh3.googleusercontent.com/pw/ACtC-3dRN-Uft0i8IaotXCZ9ci0AM2w2geeFSPQkQlqHjKLKOqD7RyTSVWAVVi7dcofb_PSWPMctDbmlo35Jt0Xv9-oppB_XUJv8kzMyvFgxtC8B3pJsSVTVjEaRYjAMMk9rx4PdC-4j7X4oyG4FCuSNBoXZ9w=w986-h657-no?authuser=0",
-    "active"),
+    "active","#",5,3),
     new Carousel("Quinze anos","Tamires",
     "https://lh3.googleusercontent.com/pw/ACtC-3fuRpNITHkxYQbADS9AjgoQwRBxzR1-_BTgFte3C-fdETYxKwE3u4GSjYhHtSxeEYEKzgnM8C4SPAElW9iYrl4-jOCojIrD7G_LZ2HZD_LCefYZseIHIv7fmhfvX-K7uk_8CwFSu29saVD-vgZvmCV1gQ=w986-h657-no?authuser=0",
-    ""),
+    "","#",13,5),
     new Carousel("Ensaio Feminino","Yasmin",
     "https://lh3.googleusercontent.com/pw/ACtC-3d3qQHGvl37uO8eZ7iMLCTxqqfWwF6tG_-lZ4qAPBmfdOlrgAyTe3WWqpLC1niEgira7pLHi70dZpKzsnsl6_9gxMC1CHoEJdFOZdkWP93m06snxrcLJoPo1lMBjkh6w5iXjWpRUjqonGGyWi87AAJuzw=w986-h657-no?authuser=0",
-    "")
+    "","#",25,14)
 ]
 export const ALBUM:Carousel[] =
 [
     new Carousel("Ensaio Feminino","Ensaio Yasmin",
     "https://alfred.alboompro.com/crop/width/640/height/480/type/jpeg/url/storage.alboom.ninja/sites/17202/albuns/718057/img_2785.jpg?t=1601589593",
-    ""),
+    "","#",30,1),
     new Carousel("Gestante","Ensaio Gestante Maisa",
     "https://alfred.alboompro.com/crop/width/640/height/480/type/jpeg/url/storage.alboom.ninja/sites/17202/albuns/718054/img_0407.jpg?t=1601589450",
-    ""),
+    "","#",80,10),
     new Carousel("Quinze anos","Bruna",
     "https://alfred.alboompro.com/crop/width/640/height/480/type/jpeg/url/storage.alboom.ninja/sites/17202/albuns/710894/img_8254.jpg?t=1600266425",
-    ""),
+    "","#",90,3),
     new Carousel("Ensaio Feminino","Larissa",
     "https://alfred.alboompro.com/crop/width/640/height/480/type/jpeg/url/storage.alboom.ninja/sites/17202/albuns/709907/img_9293.jpg?t=1600111155",
-    ""),
+    "","#",50,13),
     new Carousel("Gestante","Ensaio Gestante Paola",
     "https://alfred.alboompro.com/crop/width/640/height/480/type/jpeg/url/storage.alboom.ninja/sites/17202/albuns/718066/img_4366.jpg?t=1601593914",
-    ""),
+    "","#",25,88),
     new Carousel("Quinze anos","Ensaio Ynaiara",
     "https://alfred.alboompro.com/crop/width/640/height/480/type/jpeg/url/storage.alboom.ninja/sites/17202/albuns/709927/img_1114.jpg?t=1600112360",
-    "")
+    "","#",75,435)
 ]
 export const MENU:Menu[] =
 [
--- src/app/shared/carousel.model.ts
+++ src/app/shared/carousel.model.ts
@@ -3,7 +3,10 @@ export class Carousel
     constructor(public tipo:string,
         public descricao:string,
         public caminho:string,
-        public ativo:string) 
+        public ativo:string,
+        public link:string,
+        public likes:number,
+        public views:number) 
     {
         
     }
